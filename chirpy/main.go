8c1693d4922009c5f883fec15d301516e47c9116
Eric Schrock <eric.schrock22@gmail.com>
2024-04-10 17:57:03 -0400

    Server metrics

--- chirpy/main.go
+++ chirpy/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
 func main() {
 	log.Println("Starting server...")
 
+	var apiCfg apiConfig
 	mux := http.NewServeMux()
-	mux.Handle("/app/*", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/*", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", healthHandler)
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetHandler)
 
 	corsMux := middlewareCors(mux)
 
@@ -20,10 +28,28 @@ func main() {
 	log.Fatal(err)
 }
 
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits = 0
 }
 
 func middlewareCors(next http.Handler) http.Handler {
