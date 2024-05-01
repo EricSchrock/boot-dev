e9cb51daae27bc5812a58ace44fd1b2ea48d7910
Eric Schrock <eric.schrock22@gmail.com>
2024-04-30 20:26:57 -0400

    Bookbot part 2

--- bookbot/main.py
+++ bookbot/main.py
@@ -1,9 +1,11 @@
 import argparse
+from typing import Dict
 
 def main(path: str) -> None:
     text = read_book(path)
     word_count = count_words(text)
-    print(word_count)
+    letter_count = count_letters(text)
+    print_report(path, word_count, letter_count)
 
 def read_book(path: str) -> str:
     with open(path, 'r', encoding='utf-8') as f:
@@ -11,6 +13,26 @@ def read_book(path: str) -> str:
 
 def count_words(text: str) -> int:
     return len(text.split())
+
+def count_letters(text: str) -> Dict[str, int]:
+    text = text.lower()
+    letter_count = {}
+    for c in text:
+        if c.isalpha():
+            if not c in letter_count:
+                letter_count[c] = 0
+            letter_count[c] += 1
+    return letter_count
+
+def print_report(path: str, word_count: int, letter_count: Dict[str, int]):
+    letter_count = {k: v for k, v in sorted(letter_count.items(), key=lambda item: item[1], reverse=True)}
+
+    print(f"--- Begin report of {path} ---")
+    print(f"{word_count} words found in the document")
+    print()
+    for k, v in letter_count.items():
+        print(f"The '{k}' character was found {v} times")
+    print("--- End report ---")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
