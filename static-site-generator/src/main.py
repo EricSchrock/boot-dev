75a57d82017b7a0574ec42226ef14324d6648cb9
Eric Schrock <eric.schrock22@gmail.com>
2024-07-10 17:33:31 -0400

    SSG generate_pages_recursive

--- static-site-generator/src/main.py
+++ static-site-generator/src/main.py
@@ -26,9 +26,19 @@ def generate_page(from_path: str, template_path: str, dest_path: str):
     with open(dest_path, 'w') as f:
         f.write(page)
 
+def generate_pages_recursive(content_dir_path: str, template_path: str, dest_dir_path: str):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+
+    for item in os.listdir(content_dir_path):
+        if os.path.isfile(f"{content_dir_path}/{item}"):
+            generate_page(f"{content_dir_path}/{item}", template_path, f"{dest_dir_path}/{item.replace(".md", ".html")}")
+        else:
+            generate_pages_recursive(f"{content_dir_path}/{item}", template_path, f"{dest_dir_path}/{item}")
+
 def main():
     deploy()
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 if __name__ == "__main__":
     main()
