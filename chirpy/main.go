1b29df2b6e192ce5d40c7c498b11d6460e12e951
Eric Schrock <eric.schrock22@gmail.com>
2024-07-26 17:16:31 -0400

    Add validateChirpHandler

--- chirpy/main.go
+++ chirpy/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -23,6 +24,7 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", healthHandler)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
 	mux.HandleFunc("/api/reset", apiCfg.resetHandler)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
 
 	corsMux := middlewareCors(mux)
 
@@ -63,6 +65,45 @@ func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
 func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	cfg.fileserverHits = 0
+}
+
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	type request struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	req := request{}
+	err := decoder.Decode(&req)
+	if err != nil {
+		log.Print(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	type response struct {
+		Valid bool   `json:"valid"`
+		Error string `json:"error"`
+	}
+
+	status := http.StatusOK
+	resp := response{}
+	resp.Valid = (len(req.Body) <= 140)
+	if !resp.Valid {
+		status = http.StatusBadRequest
+		resp.Error = "Chirp is too long"
+	}
+
+	data, err := json.Marshal(resp)
+	if err != nil {
+		log.Print(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(data)
 }
 
 func middlewareCors(next http.Handler) http.Handler {
