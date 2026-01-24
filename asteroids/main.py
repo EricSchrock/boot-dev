1b4fed72d2cfc0fa51d8c7007b7dfa2a876e7595
EricSchrock <eric.schrock22@gmail.com>
2026-01-24 21:54:34 +0000

    Implement player

--- asteroids/main.py
+++ asteroids/main.py
@@ -2,6 +2,7 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -11,7 +12,12 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    Δt = 0
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    _ = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -20,11 +26,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill("black")
+        for item in updatable:
+            item.update(dt)
 
+        screen.fill("black")
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
 
-        Δt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
