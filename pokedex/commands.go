e5c270cee24d5d3d8124644a3936237f9b2a9f40
EricSchrock <eric.schrock22@gmail.com>
2026-02-28 19:52:48 +0000

    Add map and mapb commands

--- pokedex/commands.go
+++ pokedex/commands.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
 
 type cliCommand struct {
-	name        string
 	description string
 	callback    func() error
 }
@@ -15,15 +17,23 @@ var commands = map[string]cliCommand{}
 
 func registerCommands() {
 	commands["exit"] = cliCommand{
-		name: "exit",
 		description: "Exit the Pokedex",
 		callback: commandExit,
 	}
 
 	commands["help"] = cliCommand{
-		name: "exit",
 		description: "Displays a help message",
 		callback: commandHelp,
+	}
+
+	commands["map"] = cliCommand{
+		description: "Displays the next 20 map locations",
+		callback: commandMap,
+	}
+
+	commands["mapb"] = cliCommand{
+		description: "Displays the last 20 map locations",
+		callback: commandMapBack,
 	}
 }
 
@@ -38,7 +48,71 @@ func commandHelp() error {
 	fmt.Println("Usage:")
 	fmt.Println()
 	for k, v := range commands {
-		fmt.Printf("%s: %s\n", k, v.description)
+		fmt.Printf("%-4s: %s\n", k, v.description)
 	}
+	return nil
+}
+
+var next_map string = "https://pokeapi.co/api/v2/location-area?offset=0&limit=20"
+var prev_map string = ""
+
+type Areas struct {
+	Count int `json:count`
+	Next string `json:next`
+	Previous string `json:previous`
+	Results [20]Area `json:results`
+}
+
+type Area struct {
+	Name string `json:name`
+	Url string `json:url`
+}
+
+func commandMap() error {
+	if next_map == "" {
+		fmt.Println("You're on the last page")
+		return nil
+	}
+
+	return getMap(next_map)
+}
+
+func commandMapBack() error {
+	if prev_map == "" {
+		fmt.Println("You're on the first page")
+		return nil
+	}
+
+	return getMap(prev_map)
+}
+
+func getMap(link string) error {
+	res, err := http.Get(link)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("Status code: %v", res.StatusCode)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	areas := Areas{}
+	if err := json.Unmarshal(body, &areas); err != nil {
+		return err
+	}
+
+	next_map = areas.Next
+	prev_map = areas.Previous
+
+	for _, area := range areas.Results {
+		fmt.Println(area.Name)
+	}
+
 	return nil
 }
